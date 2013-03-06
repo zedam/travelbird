b485a27f234c53f0a976bbc742cc6c99097c7e1e
Ismael ZigelBaum <zedam@iMac-de-Ismael-ZigelBaum.local>
2013-03-06 12:45:48 +0100

    changes made to settings

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'myapp',
         'USER': 'root',
-        'PASSWORD': ' ',
+        'PASSWORD': 'zi7zi7',
         'HOST': '127.0.0.1',
         'PORT': ''
     }
